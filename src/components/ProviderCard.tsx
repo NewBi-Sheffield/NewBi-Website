c5dafe74a34f3cb7b9080705477b9813904c669b
SkepticalJS <isiahmukasa@gmail.com>
2026-04-01 22:08:41 +0100

    Provider card touch up

--- src/components/ProviderCard.tsx
+++ src/components/ProviderCard.tsx
@@ -7,7 +7,17 @@ function toTitleCase(s: string) {
 }
 
 function stripHtml(html: string): string {
-  return html.replace(/<[^>]*>/g, " ").replace(/\s+/g, " ").trim();
+  return html
+    .replace(/<[^>]*>/g, " ")
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&nbsp;/g, " ")
+    .replace(/\\n/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
 }
 
 type Props = {
